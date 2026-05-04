2c403cae4f13d3578a922c3c9aa9488a15eff00f
Siddhant S <siddhantsin343@gmail.com>
2026-05-04 10:22:04 -0700

    function , if-else

---
+++ 2.3 [function , if-else]/script.js
@@ -0,0 +1,34 @@
+//  create a function to add 2 number 
+
+function sum(a,b){
+    return ans = a+b
+}
+console.log( "answer is " + sum(10,2));
+
+
+//  create a function to check if a person is eligable to vote or not
+function isEligable (age){
+    if(age>=18){
+        console.log("vote")
+
+    }else{
+        console.log("not eligable");
+        
+    }}
+
+    let val = isEligable(1)
+    console.log(val);
+    
+// create a function to check if a number is odd or even
+function isEven(num){
+
+    if(num %2 == 0){
+            console.log("Even");
+            
+        
+    }else{
+        console.log("ODD");
+        
+    }
+}
+isEven(1)
