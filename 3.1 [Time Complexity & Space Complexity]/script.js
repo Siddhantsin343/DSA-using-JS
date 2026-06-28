646374a3c787ec9b7a80ef1bf2b861f9cf596586
Siddhant S <siddhantsin343@gmail.com>
2026-06-28 21:46:56 +0530

    big O notation

--- 3.1 [Time Complexity & Space Complexity]/script.js
+++ 3.1 [Time Complexity & Space Complexity]/script.js
@@ -2,3 +2,9 @@
 //  Time Complexity is != time taken by code to run 
 //  formula of binary x = log_2 n
 // if iteration is 1000 = it take 10 time 
+
+// means Binary >>> linear in term of time complexity
+
+//  To represent time Complexity we use [Big O notaion]
+
+// # Big O notation : say we measure the time complexity in worse case 
