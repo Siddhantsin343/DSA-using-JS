7f0c28ebc87f1c020ec14943d758caf8179f1ec4
Siddhant S <siddhantsin343@gmail.com>
2026-06-30 18:01:04 +0530

    Best time to buy and sell stock Q: 121

---
+++ 4.4 [Best time to buy and sell stock ]/script.js
@@ -0,0 +1,32 @@
+// Q : 121 best time to buy and sell stock
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+//  Ex 
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+ let price = [7,1,5,3,6,4]
+ 
+ function stock (price){
+    let min = price[0]
+    let maxProfit = 0
+    for(let i=1 ; i<price.length; i++){
+        if(price[i]-min > maxProfit){
+            maxProfit = price[i]-min
+        }
+        else if(price[i] < min){
+            min = price[i]
+
+        }
+    }
+    return maxProfit
+ }
+
+ console.log(stock(price))
