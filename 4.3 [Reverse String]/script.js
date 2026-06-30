92ad434a7f8b5b05fe2fd0c90da9ecefc6065880
Siddhant S <siddhantsin343@gmail.com>
2026-06-30 16:53:00 +0530

    reverse string question 344

---
+++ 4.3 [Reverse String]/script.js
@@ -0,0 +1,24 @@
+// Reverse String Leetcode Q: 344
+
+// input = ["h","e","l","l", "o"] 
+// output = ["h","e","l","l", "o"] 
+// inplace
+
+// formula : swap (n-1-i)
+
+
+let s = ["h","e","l","l", "o"]
+
+function reverseSting(s){
+
+  let length = s.length
+  let lenHalf = Math.floor(length / 2)
+
+  for(let i=0; i<lenHalf; i++){
+    let temp = s[i]
+    s[i] = s[lenHalf-1-i]
+    s[lenHalf-1-i] = temp
+  }
+
+}
+console.log(reverseSting(s));
