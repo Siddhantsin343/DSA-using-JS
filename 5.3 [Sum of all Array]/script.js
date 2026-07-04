cc4105e7335b8ab7a7743ab03bce290209254854
Siddhant S <siddhantsin343@gmail.com>
2026-07-04 21:50:33 +0530

    using Odd value

--- 5.3 [Sum of all Array]/script.js
+++ 5.3 [Sum of all Array]/script.js
@@ -8,3 +8,27 @@ function sum(n){
 
 }
 console.log(sum(arr.length -1)) 
+
+{
+    let arr = [5, 2, 0, 3, 6, 7];
+
+function ssum(n) {
+    let isOdd = (arr[n] % 2 != 0);
+
+    if (n == 0) {
+        if (isOdd) {
+            return arr[n];
+        } else {
+            return 0;
+        }
+    }
+
+    if (isOdd) {
+        return arr[n] + ssum(n - 1);
+    } else {
+        return ssum(n - 1);
+    }
+}
+
+console.log(ssum(arr.length - 1));
+}
