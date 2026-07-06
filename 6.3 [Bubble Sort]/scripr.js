c59a8e251d95af777f595f25bd39188cbfbd2d09
siddhant <siddhantsin343@gmail.com>
2026-07-06 19:30:17 +0530

    bubble sort

---
+++ 6.3 [Bubble Sort]/scripr.js
@@ -0,0 +1,26 @@
+// bubble Sort
+
+let arr = [8, 1, 3, 5];
+
+function BubbleSort(arr) {
+  let n = arr.length;
+
+  for(let i =0; i<n-1; i++ ){
+    let isSwapped = false;
+    for(let j=0; j<n-1-i; j++){
+        if(arr[j]>arr[j+1]){
+            let temp = arr[j];
+            arr[j] = arr[j+1]
+            arr[j+1]= temp
+            isSwapped = true
+        }
+
+    }
+
+    if(!isSwapped) break;
+  }
+  return arr
+}
+
+let result = BubbleSort(arr);
+console.log(result);
