fc2727a86945abea39ba30fc37562db02335af41
Siddhant S <siddhantsin343@gmail.com>
2026-07-08 13:33:46 +0530

    Insertion sort

--- 6.5 [Insertion sort]/script.js
+++ 6.5 [Insertion sort]/script.js
@@ -2,21 +2,21 @@
 
 // Insertion sort is a simple, in-place algorithm that builds a sorted array one element at a time. It works exactly like sorting a hand of playing cards: you take one card from the unsorted pile, compare it to the sorted cards, and insert it into its correct position.
 
-let arr = [7, 4, 2, 5, 1, 2];
+let arr =[7,4,2,5,1,2];
 
-function insertionSort(a) {
-  let n = a.length;
-  for (let i = 1; i < n; i++) {
-    let curr = a[i];
-    let prev = i - 1;
-    while (a[prev] > curr && prev >= 0) {
-      a[prev + 1] = a[prev];
-      prev--;
+function insertionSort(a){
+    let n = a.length;
+    for(let i=1; i<n; i++){
+        let curr = a[i]
+        let prev = i-1
+        while(a[prev]> curr && prev >=0){
+            a[prev+1] = a[prev]
+            prev --;
+        }   
+        a[prev+1] =curr
     }
-    a[prev + 1] = curr;
-  }
-  return arr;
-}
+    return arr;
+} 
 
-let reult = insertionSort(arr);
+let reult = insertionSort(arr)
 console.log(reult);
---
+++ 6.6 [Merge Sort]/script.js
@@ -0,0 +1,8 @@
+// Mergte Sort 
+
+// leet code Question 912
+
+// / there is 2 phase 
+// 1. divide 
+// 2. merge 
+
